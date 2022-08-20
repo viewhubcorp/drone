9d1879e2027df4a75130c2ba095d966b8ba56bd1
Slavik <mac@Macs-MBP.wlan>
2022-08-21 00:57:53 +0300

    fix

--- web/app/Lib/Wifi.php
+++ web/app/Lib/Wifi.php
@@ -88,7 +88,7 @@ class Wifi {
         if(empty($ret)){
             return '';
         }else{
-            dd(rtrim(explode('ESSID:"', $ret)[1],'"\n'));
+            dd(rtrim(explode('ESSID:"', $ret)[1],"\"\n"));
             return rtrim(explode('ESSID:"', $ret)[1],'"');
         }
     }
